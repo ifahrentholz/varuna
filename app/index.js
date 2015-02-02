1b00427d3e76a034ece35577b1fb5c769de1f2fe
timbuktuuu <tim.wiedenbeck@gmail.com>
2015-02-02 18:54:32 +0100

    Revert "add Bootstrap and Modernizr Support on install"
    This reverts commit 3e2c5970b823c5c8ef57f74acaf47a21e76aaf6f.

--- app/index.js
+++ app/index.js
@@ -7,89 +7,25 @@ module.exports = yeoman.generators.Base.extend({
   initializing: function () {
     this.pkg = require('../package.json');
   },
-  
-  askFor: function () {
+
+  prompting: function () {
     var done = this.async();
 
-    // welcome message
-    if (!this.options['skip-welcome-message']) {
-      this.log(yosay(
-        'Welcome to the frontend generator by ]init['
-      ));
-      this.log(chalk.magenta(
-        'Out of the box I include HTML5 Boilerplate, jQuery, and a ' +
-        'Gruntfile.js to build your app.'
-      ));
-    }
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the frontend generator by ]init['
+    ));
 
     var prompts = [{
-      type: 'checkbox',
-      name: 'features',
-      message: 'What more would you like?',
-      choices: [{
-        name: 'Bootstrap',
-        value: 'includeBootstrap',
-        checked: false
-      },{
-        name: 'Sass',
-        value: 'includeSass',
-        checked: false
-      },{
-        name: 'Modernizr',
-        value: 'includeModernizr',
-        checked: true
-      }]
-    }, {
-      when: function (answers) {
-        return answers && answers.features &&
-          answers.features.indexOf('includeSass') !== -1;
-      },
-      type: 'confirm',
-      name: 'libsass',
-      value: 'includeLibSass',
-      message: 'Would you like to use libsass? Read up more at \n' +
-        chalk.green('https://github.com/andrew/node-sass#node-sass'),
-      default: false
+      name: 'appName',
+      message: 'What is your app\'s name ?',
     }];
 
-    this.prompt(prompts, function (answers) {
-      var features = answers.features;
-
-      function hasFeature(feat) {
-        return features && features.indexOf(feat) !== -1;
-      }
-
-      this.includeSass = hasFeature('includeSass');
-      this.includeBootstrap = hasFeature('includeBootstrap');
-      this.includeModernizr = hasFeature('includeModernizr');
-
-      this.includeLibSass = answers.libsass;
-      this.includeRubySass = !answers.libsass;
+    this.prompt(prompts, function (props) {
+      this.appName = props.appName;
 
       done();
     }.bind(this));
-  },
-  
-  bower: function () {
-    var bower = {
-      name: this._.slugify(this.appname),
-      private: true,
-      dependencies: {}
-    };
-
-    if (this.includeBootstrap) {
-      var bs = 'bootstrap' + (this.includeSass ? '-sass-official' : '');
-      bower.dependencies[bs] = "~3.2.0";
-    } else {
-      bower.dependencies.jquery = "~1.11.1";
-    }
-
-    if (this.includeModernizr) {
-      bower.dependencies.modernizr = "~2.8.2";
-    }
-
-    this.copy('bowerrc', '.bowerrc');
-    this.write('bower.json', JSON.stringify(bower, null, 2));
   },
 
   writing: {
@@ -103,6 +39,10 @@ module.exports = yeoman.generators.Base.extend({
       this.fs.copy(
         this.templatePath('_package.json'),
         this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
       );
     },
 
